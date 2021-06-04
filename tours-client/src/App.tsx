3e4fc97a6e30cf59555eebdb12c83f9fc2ab7b55
DVGY <gaurav.gydv@gmail.com>
2021-06-04 16:04:05 +0530

    add login form

--- tours-client/src/App.tsx
+++ tours-client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
 
 function App(): JSX.Element {
   return (
     <div className='App'>
       <Navbar />
+      <Login />
     </div>
   );
 }
--- tours-client/src/components/Login.tsx
+++ tours-client/src/components/Login.tsx
@@ -1,7 +1,108 @@
 import React from 'react';
 
-const Login = () => {
-  return <div></div>;
+import { useState } from 'react';
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  InputLeftAddon,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  Text,
+  FormHelperText,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { EmailIcon, ViewIcon, LockIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+const Login = (): JSX.Element => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  return (
+    <Flex
+      flexDirection='column'
+      width='100wh'
+      height='100vh'
+      backgroundColor='gray.200'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Stack
+        flexDir='column'
+        mb='2'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Box maxW={{ base: '95%', sm: '90%', md: '500px' }}>
+          <form>
+            <Stack
+              spacing={4}
+              p='1rem'
+              backgroundColor='whiteAlpha.900'
+              boxShadow='md'
+            >
+              <Text fontSize='3xl' color='gray.300'>
+                Login
+              </Text>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement pointerEvents='none'>
+                    <EmailIcon color='gray.300' />
+                  </InputLeftElement>
+                  <Input type='email' placeholder='email address' />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement pointerEvents='none' color='gray.300'>
+                    <LockIcon color='gray.300' />
+                  </InputLeftElement>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder='Password'
+                  />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handleShowClick}>
+                      {showPassword ? (
+                        <ViewOffIcon color='gray.300' />
+                      ) : (
+                        <ViewIcon color='gray.300' />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Button
+                borderRadius={0}
+                type='submit'
+                variant='solid'
+                colorScheme='teal'
+                width='full'
+              >
+                LOGIN
+              </Button>
+              <Flex justifyContent='space-between' alignContent='space-between'>
+                <Link>
+                  <Text fontSize='xs'>Forgot password? </Text>
+                </Link>
+                <Link href='#'>
+                  <Text fontSize='xs'>Sign Up </Text>
+                </Link>
+              </Flex>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default Login;
