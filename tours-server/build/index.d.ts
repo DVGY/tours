d02f7f8c8d15cdfc7885844579b01591e4b47b8d
DVGY <gaurav.gydv@gmail.com>
2021-07-14 10:26:51 +0530

    remove build folder

--- tours-server/build/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { APIFeatures } from './utils/APIFeatures';
-export { catchAsync, asyncController } from './utils/catchAsync';
-export { errorHandler, IAppError } from './utils/errorHandler';
--- tours-server/build/utils/email.d.ts
+++
@@ -1,7 +0,0 @@
-interface IEmailOptions {
-    email: string;
-    subject: string;
-    message: string;
-}
-export declare const sendEmail: ({ email, subject, message, }: IEmailOptions) => Promise<void>;
-export {};
