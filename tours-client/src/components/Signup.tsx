1dfbb12b4d19f0d0d16575cbf030780b7ff21bbe
DVGY <gaurav.gydv@gmail.com>
2021-08-26 21:24:05 +0530

    responsive login page

--- tours-client/src/components/Signup.tsx
+++ tours-client/src/components/Signup.tsx
@@ -11,6 +11,7 @@ import {
   FormControl,
   Text,
   InputRightElement,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { EmailIcon, ViewIcon, LockIcon, ViewOffIcon } from '@chakra-ui/icons';
 import {
@@ -18,6 +19,7 @@ import {
   useHistory,
   useLocation,
 } from 'react-router-dom';
+import { FiUser } from 'react-icons/fi';
 
 type LocationState = {
   from: {
@@ -42,6 +44,11 @@ const Signup = (): JSX.Element => {
   const { loading } = useTypedSelector((reduxStore) => reduxStore.auth);
   const { state } = useLocation<LocationState>();
   const histoy = useHistory();
+  const buttonSize = useBreakpointValue({
+    base: 'sm',
+    md: 'md',
+    lg: 'lg',
+  });
 
   const { username, email, password, passwordConfirm } = formData;
 
@@ -74,37 +81,42 @@ const Signup = (): JSX.Element => {
   return (
     <Flex
       flexDirection='column'
-      width='100wh'
       height='100vh'
       backgroundColor='gray.200'
       justifyContent='start'
       alignItems='center'
+      marginTop={[16, 16, 16, 20, 20, 20]}
       py={4}
     >
-      <Stack
-        flexDir='column'
-        mb='2'
-        justifyContent='center'
-        alignItems='center'
-        mt={24}
-      >
-        <Box maxW={{ base: '95%', sm: '90%', md: '500px' }}>
+      <Stack flexDir='column' mb='2' mt={24}>
+        <Box>
           <form onSubmit={onSubmit}>
             <Stack
               spacing={4}
-              p='1rem'
+              px={[4, 4, 4, 6, 6, 6]}
+              py={[4, 4, 4, 6, 6, 6]}
               backgroundColor='whiteAlpha.900'
               boxShadow='md'
             >
-              <Text fontSize='3xl' textAlign='center' color='teal.400'>
+              <Text
+                fontSize={['3xl', '3xl', '3xl', '3xl', '4xl', '4xl']}
+                textAlign='center'
+                color='teal.400'
+              >
                 Sign Up
               </Text>
               <FormControl>
                 <InputGroup>
-                  <InputLeftElement pointerEvents='none'>
-                    <EmailIcon color='teal.400' />
+                  <InputLeftElement
+                    top={'50%'}
+                    transform={'translate(0%,-50%)'}
+                    pointerEvents='none'
+                  >
+                    <FiUser stroke='#38B2AC' fill='#38B2AC' />
                   </InputLeftElement>
                   <Input
+                    py={[4, 4, 4, 6, 6, 8]}
+                    fontSize={['sm', 'sm', 'lg', 'lg', 'xl', '2xl']}
                     type='text'
                     name='username'
                     value={username}
@@ -115,10 +127,19 @@ const Signup = (): JSX.Element => {
               </FormControl>
               <FormControl>
                 <InputGroup>
-                  <InputLeftElement pointerEvents='none'>
-                    <EmailIcon color='teal.400' />
+                  <InputLeftElement
+                    top={'50%'}
+                    transform={'translate(0%,-50%)'}
+                    pointerEvents='none'
+                  >
+                    <EmailIcon
+                      fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                      color='teal.400'
+                    />
                   </InputLeftElement>
                   <Input
+                    py={[4, 4, 4, 6, 6, 8]}
+                    fontSize={['sm', 'sm', 'lg', 'lg', 'xl', '2xl']}
                     type='email'
                     name='email'
                     value={email}
@@ -129,22 +150,42 @@ const Signup = (): JSX.Element => {
               </FormControl>
               <FormControl>
                 <InputGroup>
-                  <InputLeftElement pointerEvents='none' color='gray.300'>
-                    <LockIcon color='teal.400' />
+                  <InputLeftElement
+                    top={'50%'}
+                    transform={'translate(0%,-50%)'}
+                    pointerEvents='none'
+                    color='gray.300'
+                  >
+                    <LockIcon
+                      fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                      color='teal.400'
+                    />
                   </InputLeftElement>
                   <Input
+                    py={[4, 4, 4, 6, 6, 8]}
+                    fontSize={['sm', 'sm', 'lg', 'lg', 'xl', '2xl']}
                     type={showPassword ? 'text' : 'password'}
                     placeholder='Password'
                     name='password'
                     value={password}
                     onChange={onChange}
                   />
-                  <InputRightElement width='4.5rem'>
+                  <InputRightElement
+                    top={'50%'}
+                    transform={'translate(0%,-50%)'}
+                    width='4.5rem'
+                  >
                     <Button h='1.75rem' size='sm' onClick={handleShowClick}>
                       {showPassword ? (
-                        <ViewOffIcon color='teal.400' />
+                        <ViewOffIcon
+                          fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                          color='teal.400'
+                        />
                       ) : (
-                        <ViewIcon color='teal.400' />
+                        <ViewIcon
+                          fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                          color='teal.400'
+                        />
                       )}
                     </Button>
                   </InputRightElement>
@@ -152,22 +193,42 @@ const Signup = (): JSX.Element => {
               </FormControl>
               <FormControl>
                 <InputGroup>
-                  <InputLeftElement pointerEvents='none' color='gray.300'>
-                    <LockIcon color='teal.400' />
+                  <InputLeftElement
+                    top={'50%'}
+                    transform={'translate(0%,-50%)'}
+                    pointerEvents='none'
+                    color='gray.300'
+                  >
+                    <LockIcon
+                      fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                      color='teal.400'
+                    />
                   </InputLeftElement>
                   <Input
+                    py={[4, 4, 4, 6, 6, 8]}
+                    fontSize={['sm', 'sm', 'lg', 'lg', 'xl', '2xl']}
                     type={showPassword ? 'text' : 'password'}
                     placeholder='Confirm Password'
                     name='passwordConfirm'
                     value={passwordConfirm}
                     onChange={onChange}
                   />
-                  <InputRightElement width='4.5rem'>
+                  <InputRightElement
+                    top={'50%'}
+                    transform={'translate(0%,-50%)'}
+                    width='4.5rem'
+                  >
                     <Button h='1.75rem' size='sm' onClick={handleShowClick}>
                       {showPassword ? (
-                        <ViewOffIcon color='teal.400' />
+                        <ViewOffIcon
+                          fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                          color='teal.400'
+                        />
                       ) : (
-                        <ViewIcon color='teal.400' />
+                        <ViewIcon
+                          fontSize={['xs', 'xs', 'xs', 'md', 'xl', 'xl']}
+                          color='teal.400'
+                        />
                       )}
                     </Button>
                   </InputRightElement>
@@ -179,19 +240,27 @@ const Signup = (): JSX.Element => {
                 variant='solid'
                 colorScheme='teal'
                 width='full'
+                size={buttonSize}
               >
                 SIGN UP
               </Button>
               <Flex justifyContent='space-between' alignContent='space-between'>
                 <ReactRouterLink to='/forgot-password'>
-                  <Text fontSize='xs'>Forgot password? </Text>
+                  <Text fontSize={['xs', 'xs', 'xs', 'md', 'md', 'md']}>
+                    Forgot password?{' '}
+                  </Text>
                 </ReactRouterLink>
                 <ReactRouterLink to='/login'>
-                  <Text fontSize='xs'>Login</Text>
+                  <Text fontSize={['xs', 'xs', 'xs', 'md', 'md', 'md']}>
+                    Login
+                  </Text>
                 </ReactRouterLink>
               </Flex>
               <Flex justifyContent='center' alignContent='space-between'>
-                <Text textAlign='center' fontSize='xs'>
+                <Text
+                  textAlign='center'
+                  fontSize={['xs', 'xs', 'xs', 'md', 'md', 'md']}
+                >
                   Or
                 </Text>
               </Flex>
@@ -209,6 +278,7 @@ const Signup = (): JSX.Element => {
                 }}
                 colorScheme='teal'
                 width='full'
+                size={buttonSize}
               >
                 GUEST LOGIN
               </Button>
